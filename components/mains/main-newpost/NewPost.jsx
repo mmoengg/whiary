d00fd4d9196af8d62ed64d46f20b53f539e66173
mmoengg <mmoengg@gmail.com>
2024-07-03 14:43:09 +0900

    fix: main - new post add

--- components/mains/main-newpost/NewPost.jsx
+++ components/mains/main-newpost/NewPost.jsx
@@ -1,7 +1,21 @@
+import Image from 'next/image';
+
 export default function NewPost() {
 	return (
-		<div className="w-full h-full max-h-[70px] rounded-2xl bg-white">
-			<div>Recommend</div>
+		<div className="px-6 py-3 w-full h-full max-h-[70px] rounded-2xl bg-white flex items-center justify-between">
+			<div className="text-xs">
+				<p className="font-bold">New post</p>
+				<p>오늘 즐긴 위스키 기록하기</p>
+			</div>
+			<div className="cursor-pointer">
+				<Image
+					src=""
+					alt="profile"
+					width={35}
+					height={35}
+					className="rounded-full bg-black"
+				></Image>
+			</div>
 		</div>
 	);
 }
