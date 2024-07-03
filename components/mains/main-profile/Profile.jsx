4601eb8a738d11dcc6a4c81a196c4ef144c8dd57
mmoengg <mmoengg@gmail.com>
2024-07-03 14:22:18 +0900

    fix: main layout add

--- components/mains/main-profile/Profile.jsx
+++ components/mains/main-profile/Profile.jsx
@@ -1,3 +1,21 @@
+import Image from 'next/image';
+
 export default function Profile() {
-	return <div>Profile</div>;
+	return (
+		<div className="w-full flex items-center justify-between">
+			<div className="text-white">
+				<p>Hi 🖐️ Moeng!</p>
+				<p>Welcome back</p>
+			</div>
+			<div>
+				<Image
+					src="/images/sik111.png"
+					alt="profile"
+					width={56}
+					height={56}
+					className="rounded-full"
+				/>
+			</div>
+		</div>
+	);
 }
--- layouts/Layout.jsx
+++ layouts/Layout.jsx
@@ -1,13 +1,46 @@
+import Image from 'next/image';
+
 export default function Layout({ children }) {
 	return (
 		<div className="w-screen h-screen flex items-center justify-center">
 			<div className="w-full h-full max-w-[393px] p-5 bg-black flex flex-col items-center justify-end">
 				{children}
 				<ul className="w-full h-[65px] flex items-center justify-between">
-					<li className="px-6 py-2.5 rounded-3xl bg-white">HOME</li>
-					<li className="px-6 py-2.5 rounded-3xl text-white">LIST</li>
-					<li className="px-6 py-2.5 rounded-3xl text-white">WHIS</li>
-					<li className="px-6 py-2.5 rounded-3xl text-white">MY</li>
+					<li className="gnb gnb--active">
+						<Image
+							src="/images/svg/i-main.svg"
+							width={14}
+							height={14}
+							alt="home"
+						/>
+					</li>
+					<li className="gnb">
+						{' '}
+						<Image
+							src="/images/svg/i-list.svg"
+							width={14}
+							height={14}
+							alt="home"
+						/>
+					</li>
+					<li className="gnb">
+						{' '}
+						<Image
+							src="/images/svg/i-wish.svg"
+							width={14}
+							height={14}
+							alt="home"
+						/>
+					</li>
+					<li className="gnb">
+						{' '}
+						<Image
+							src="/images/svg/i-mypage.svg"
+							width={14}
+							height={14}
+							alt="home"
+						/>
+					</li>
 				</ul>
 			</div>
 		</div>
