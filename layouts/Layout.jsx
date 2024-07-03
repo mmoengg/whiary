1083dfe41cb18f37002537954bf8517467493b7a
mmoengg <mmoengg@gmail.com>
2024-07-03 13:51:55 +0900

    fix: layouts add

--- layouts/Layout.jsx
+++ layouts/Layout.jsx
@@ -1,8 +1,15 @@
 export default function Layout({ children }) {
-  return (
-    <div className="w-screen h-screen flex items-center justify-center">
-        {children}
-        <div></div>
-    </div>
-  )
+	return (
+		<div className="w-screen h-screen flex items-center justify-center">
+			<div className="w-full h-full max-w-[393px] p-5 bg-black flex flex-col items-center justify-end">
+				{children}
+				<ul className="w-full flex items-center justify-between">
+					<li className="px-6 py-2 rounded-3xl bg-white">HOME</li>
+					<li className="px-6 py-2 rounded-3xl text-white">LIST</li>
+					<li className="px-6 py-2 rounded-3xl text-white">WHIS</li>
+					<li className="px-6 py-2 rounded-3xl text-white">MY</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,10 @@
-import Layout from "@/layouts/Layout";
-import "@/styles/globals.css";
+import Layout from '@/layouts/Layout';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <Layout> 
-        <Component {...pageProps} />;
-    </Layout>
-  )
+	return (
+		<Layout>
+			<Component {...pageProps} />;
+		</Layout>
+	);
 }
