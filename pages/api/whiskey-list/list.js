b685d8e3876eac4ff4f323104d505c898beade2b
mmoengg <mmoengg@gmail.com>
2024-07-04 10:49:30 +0900

    fix: env 설정 수정

--- pages/api/whiskey-list/list.js
+++ pages/api/whiskey-list/list.js
@@ -1,8 +1,8 @@
 import { MongoClient } from 'mongodb';
 
 async function handler(req, res) {
-	const client = await MongoClient.connect(process.env.MONGO_URI);
-	const db = client.db(process.env.MONGO_DB);
+	const client = await MongoClient.connect(process.env.NEXT_PUBLIC_MONGO_URI);
+	const db = client.db(process.env.NEXT_PUBLIC_MONGO_DB);
 	const list = await db.collection('list').find().toArray();
 	res.status(200).json({ list });
 	console.log('연결 성공');
