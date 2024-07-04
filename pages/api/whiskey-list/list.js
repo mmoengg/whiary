7fa1a7b9c1e568a55e501f0819f16e4709d59b39
mmoengg <mmoengg@gmail.com>
2024-07-04 11:14:29 +0900

    chore: test

--- pages/api/whiskey-list/list.js
+++ pages/api/whiskey-list/list.js
@@ -1,12 +1,12 @@
 import { MongoClient } from 'mongodb';
 
-async function handler(req, res) {
-	const client = await MongoClient.connect(process.env.NEXT_PUBLIC_MONGO_URI, {});
+const connectDB = async (req, res) => {
+	const client = await MongoClient.connect(process.env.NEXT_PUBLIC_MONGO_URI);
 	const db = client.db(process.env.NEXT_PUBLIC_MONGO_DB);
 	const list = await db.collection('list').find().toArray();
 	res.status(200).json({ list });
 	console.log('연결 성공');
 	client.close();
-}
+};
 
-export default handler;
+export default connectDB;
